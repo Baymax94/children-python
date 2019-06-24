8b7c3f1fa939c4a1305e35c75028cfcf35ffb4c3
Baymax <baixuelin12hit@outlook.com>
2019-06-24 20:39:37 +0800

    turtle模块
    海龟画图

---
+++ 趣学Python编程/rectangle.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Pen()
+t.forward(100)  
+t.left(90)   
+t.forward(50)  
+t.left(90)  
+t.forward(100)  
+t.left(90)  
+t.forward(50)  
+t.left(90) 
---
+++ 趣学Python编程/triangle.py
@@ -0,0 +1,8 @@
+import turtle
+t = turtle.Pen()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
