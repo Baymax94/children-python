21cf6bae40dfc8dcd0562c040cf0011a9bcc96e0
Baymax <baixuelin12hit@outlook.com>
2019-11-18 16:10:45 +0800

    tensorflow

--- LJ人工智能编程 初中版/15_1_01.py
+++ LJ人工智能编程 初中版/15_1_01.py
@@ -4,26 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data=pd.read_csv('test.csv')
-x=data.angle
-y=data.speed
+data = pd.read_csv('test.csv')
+x = data.angle
+y = data.speed
 
 # 构建神经元模型
-model=keras.Sequential()
-model.add(layers.Dense(1,input_dim=1))
+model = keras.Sequential()
+model.add(layers.Dense(1, input_dim=1))
 
 # 打印model.summary()查看模型的参数
 print(model.summary())
 
 # 编辑模型
-model.compile(optimizer='adam',loss='mse')
+model.compile(optimizer='adam', loss='mse')
 
 # 训练模型
-history=model.fit(x,y,batch_size=100,epochs=10000)
+history = model.fit(x, y, batch_size=100, epochs=10000)
 
 # 绘制数据
-y_predict=model.predict(x)
-plt.scatter(x,y)
-plt.plot(x,y_predict,c='r')
+y_predict = model.predict(x)
+plt.scatter(x, y)
+plt.plot(x, y_predict, c='r')
 
 # 缺少tensorflow环境
