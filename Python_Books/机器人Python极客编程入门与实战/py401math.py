386b9c036a9d4f8675ef96e41a656a84522223e1
Baymax <baixuelin12hit@outlook.com>
2019-11-19 15:18:45 +0800

    第3章阅读

---
+++ Python_Books/机器人Python极客编程入门与实战/py401math.py
@@ -0,0 +1,32 @@
+# 3-1
+# 1
+print('\n#1')
+x = 10
+y = 22
+z = 35
+print('x,y,z', x, y, z)
+
+# 2
+print('\n#2')
+a = x+y
+b = x-y
+c = z-x*y
+print('a=x+y,', a)
+print('b=x-y,', b)
+print('c=z-x*y,', c)
+
+# 3
+print('\n#3')
+a = z/x
+b = z//x
+c = z % x
+print('a=z/x,', a)
+print('b=z//x,', b)
+print('c=z%x,', c)
+
+# 4
+print('\n#4')
+a = x**2
+b = x**3
+print('a=x**2,', a)
+print('b=x**3,', b)
